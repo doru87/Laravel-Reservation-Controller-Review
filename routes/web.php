55b34159975d5c62ab84783b46c806d4b34805cd
modestasv <info@modestasv.lt>
2024-11-13 16:16:22 +0200

    Added route just in case we want to run it

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserReservationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('reservation', UserReservationController::class);
